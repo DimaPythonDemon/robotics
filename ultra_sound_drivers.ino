6f8fd895449c83ff9fe2e5984591fa59be87ca34
maksimod <58116860+maksimod@users.noreply.github.com>
2024-04-22 10:42:47 +0300

    add r ultra sound sensor

--- ultra_sound_drivers.ino
+++ ultra_sound_drivers.ino
@@ -1,7 +1,10 @@
-#define PIN_TRIG 12
-#define PIN_ECHO 11
+#define F_PIN_TRIG 12
+#define F_PIN_ECHO 11
 
-long duration, cm;
+#define R_PIN_TRIG 9
+#define R_PIN_ECHO 10
+
+long duration,cm;
 
 void setup() {
 
@@ -9,31 +12,38 @@ void setup() {
 
   Serial.begin (9600);
   //Определяем вводы и выводы
-  pinMode(PIN_TRIG, OUTPUT);
-  pinMode(PIN_ECHO, INPUT);
+  pinMode(F_PIN_TRIG, OUTPUT);
+  pinMode(F_PIN_ECHO, INPUT);
+
+  pinMode(R_PIN_TRIG, OUTPUT);
+  pinMode(R_PIN_ECHO, INPUT);
 }
 
 void loop() {
-
   // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
-
-  digitalWrite(PIN_TRIG, LOW);
+  digitalWrite(R_PIN_TRIG, LOW);
+  digitalWrite(F_PIN_TRIG, LOW);
   delayMicroseconds(5);
-  digitalWrite(PIN_TRIG, HIGH);
+  digitalWrite(F_PIN_TRIG, HIGH);
+  digitalWrite(R_PIN_TRIG, HIGH);
 
   // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы с частотой 40 КГц.
   delayMicroseconds(10);
-  digitalWrite(PIN_TRIG, LOW);
+  digitalWrite(F_PIN_TRIG, LOW);
+  digitalWrite(R_PIN_TRIG, LOW);
 
   //  Время задержки акустического сигнала на эхолокаторе.
-  duration = pulseIn(PIN_ECHO, HIGH);
+  f_duration = pulseIn(F_PIN_ECHO, HIGH);
+  r_duration = pulseIn(R_PIN_ECHO, HIGH);
 
   // Теперь осталось преобразовать время в расстояние
-  cm = (duration / 2) / 29.1;
+  f_cm = (f_duration / 2) / 29.1;
+  r_cm = (r_duration / 2) / 29.1;
 
-  Serial.print("Расстояние до объекта: ");
-  Serial.print(cm);
-  Serial.println(" см.");
+  Serial.print("F: ");
+  Serial.print(f_cm);
+  Serial.print(" R: ");
+  Serial.println(r_cm);
 
   // Задержка между измерениями для корректной работы скеча
   delay(250);
