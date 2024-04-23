98ce8972f65c6a79539d986707e5be5d01870633
maksimod <58116860+maksimod@users.noreply.github.com>
2024-04-23 18:14:14 +0300

    optimize code, make it working for accums

--- ultra_sound_drivers.ino
+++ ultra_sound_drivers.ino
@@ -13,9 +13,9 @@ char f = 'f';
 char r = 'r';
 char l = 'l';
 
-double driver_k = 0.96;
+double all_speed_k = 1;
 
-int turn_spd = 60;
+int turn_spd = 70;
 
 // блоки прохождения программы
 boolean b1 = true;
@@ -30,13 +30,13 @@ boolean right_cubs[7];
 int right_cube_counter = 0;
 //слева
 boolean left_cubs[7];
-left_cube_counter = 0;
+int left_cube_counter = 0;
 
-cur_perec_counter = 0;
+int cur_perec_counter = 0;
 
 
 //расстояние для бочки
-right_circle_distance = 12;
+int right_circle_distance = 9;
 
 void setup() {
   //инициализируем взаимодействие по последовательному порту
@@ -62,17 +62,16 @@ void setup() {
 }
 
 void circle(int r_cm, int turn_spd){
-  if (r_cm > tight_circle_distance){
+  if (r_cm > right_circle_distance){
     right(turn_spd);
   }
   else{
-    forward(70);
+    forward(80);
   }
 }
 
 int measureCm(char side, int prev_cm){
-  int response_rate = 10;
-  
+  int response_rate = 50;
   if (side=='f'){
     if (millis()-real_f_ultra_time>response_rate){
       long f_duration,f_cm;
@@ -80,19 +79,11 @@ int measureCm(char side, int prev_cm){
       digitalWrite(F_PIN_TRIG, LOW);
       delayMicroseconds(5);
       digitalWrite(F_PIN_TRIG, HIGH);
-  
-      // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы с частотой 40 КГц.
       delayMicroseconds(10);
       digitalWrite(F_PIN_TRIG, LOW);
-  
-      //  Время задержки акустического сигнала на эхолокаторе.
       f_duration = pulseIn(F_PIN_ECHO, HIGH);
-    
-      // Теперь осталось преобразовать время в расстояние
       f_cm = (f_duration / 2) / 29.1;
-
       real_f_ultra_time = millis();
-
       return f_cm;
     }
     else return prev_cm;
@@ -100,23 +91,14 @@ int measureCm(char side, int prev_cm){
   else{
     if (millis()-real_r_ultra_time>response_rate){
       long r_duration,r_cm;
-      // Сначала генерируем короткий импульс длительностью 2-5 микросекунд.
       digitalWrite(R_PIN_TRIG, LOW);
       delayMicroseconds(5);
-      digitalWrite(R_PIN_TRIG, HIGH);  
-  
-      // Выставив высокий уровень сигнала, ждем около 10 микросекунд. В этот момент датчик будет посылать сигналы с частотой 40 КГц.
+      digitalWrite(R_PIN_TRIG, HIGH);
       delayMicroseconds(10);
       digitalWrite(R_PIN_TRIG, LOW);
-  
-      //  Время задержки акустического сигнала на эхолокаторе.
-      r_duration = pulseIn(R_PIN_ECHO, HIGH);
-    
-      // Теперь осталось преобразовать время в расстояние
+      r_duration = pulseIn(R_PIN_ECHO, HIGH);  
       r_cm = (r_duration / 2) / 29.1;
-
       real_r_ultra_time = millis();
-
       return r_cm;
     }
     else return prev_cm;
@@ -127,21 +109,21 @@ void ride(char motor, int spd){
   if (motor == 'l'){
     if (spd<0){
       digitalWrite(7,0);
-      analogWrite(6, -spd);
+      analogWrite(6, -spd*all_speed_k);
     }
     else{
       digitalWrite(7,1);
-      analogWrite(6, spd);
+      analogWrite(6, spd*all_speed_k);
     }
   }
   else {
     if (spd<0){
       digitalWrite(4,0);
-      analogWrite(5, -spd*driver_k);
+      analogWrite(5, -spd*all_speed_k );
     }
     else{
       digitalWrite(4,1);
-      analogWrite(5, spd*driver_k);
+      analogWrite(5, spd*all_speed_k );
     }    
   }
 }
@@ -174,10 +156,10 @@ void pidLineRegul(int spd = 110){
   //левый на линии, правый - нет
   else if (digitalRead(A2)){
     ride(r, spd);
-    ride(l, spd*driver_k);
+    ride(l, spd);
   }
   else if (digitalRead(A1)){
-    ride(r, spd*driver_k);
+    ride(r, spd);
     ride(l, spd);
   }
   else{
@@ -194,57 +176,94 @@ boolean perec(){
 
 void stp() {forward(0);}
 
+//для того чтобы робот понимал, что он поворачивается
+boolean start_turning = false;
+unsigned long start_turning_real_time;
+
 void loop() {
-  if (b1){
-    Serial.println("PDREG");
-    f_cm = measureCm(f, f_cm);
-    if (f_cm > 15){
-      pidLineRegul(80);  
-    }
-    else{
-      b1 = false;
-      b2 = true;
-      stp();
-    }
-  }
-  else if (b2){
-    Serial.println("turning");
-    left(95);
-    delay(300);
-    stp();
-    delay(1000);
-    b2 = false;
-    b3 = true;
-  }
-  else if (b3){
-    Serial.println("CIRCLE");
-    r_cm = measureCm(r, r_cm);
-    circle(r_cm, turn_spd);  
-    if (perec()){
-      forward(100);
-      delay(200);
-      stp();
-      delay(500);
-      cur_perec_counter+=1;
-      b3 = false;
-      b4 = true;
-    }
-  }
-  else if (b4){
-    f_cm = measureCm(f, f_cm);
-    if (f_cm > 30){
-      left(85);
-    }
-    else{
-      stp();
-      right_cubs[right_cube_counter] = true;
-      right_cube_counter += 1;
-      b4 = false;
-      if (cur_perec_counter<7) b3 = true;
-      else b5 = true;
-      while (measureCm(f, f_cm)>right_circle_distance)right(85);
-      stp();
-      delay(500);
-    }
-  }
+//  left(60);
+//  delay(1000);
+//  stp();
+//  delay(500);
+//  right(60);
+//  delay(1000);
+//  stp();
+//  delay(500);
+  Serial.print("L:");
+  Serial.print(digitalRead(A1));
+  Serial.print("R:");
+  Serial.println(digitalRead(A2));
+  
+
+//  pidLineRegul(60);
+//  if (b1){
+//    Serial.println("PDREG");
+//    f_cm = measureCm(f, f_cm);
+//    if (f_cm > 10){
+//      pidLineRegul(80);  
+//    }
+//    else{
+//      b1 = false;
+//      b2 = true;
+//      stp();
+//      delay(500);
+//    }
+//  }
+//  else if (b2){
+//    Serial.println("turning");
+//    left(90);
+//    delay(500);
+//    stp();
+//    delay(1000);
+//    b2 = false;
+//    b3 = true;
+//  }
+//  else if (b3){
+//    Serial.println("CIRCLE");
+//    r_cm = measureCm(r, r_cm);
+//    circle(r_cm, turn_spd);  
+//    if (perec()){
+//      forward(100);
+//      delay(200);
+//      stp();
+//      delay(500);
+//      cur_perec_counter+=1;
+//      b3 = false;
+//      b4 = true;
+//      start_turning_real_time = millis();
+//    }
+//  }
+//  else if (b4){
+//    f_cm = measureCm(f, f_cm);
+//    r_cm = measureCm(r, r_cm);
+//    if (millis() - start_turning_real_time > 800){
+//      //замедляемся чтоб не проебать поворот
+//      left(74);
+//      if (r_cm < 15){
+//            stp();
+//            delay(500);
+//            b4 = false;
+//            if (cur_perec_counter<7) b3 = true;
+//            else b5 = true;      
+//      } 
+//    }
+//    else{
+//      if (f_cm > 30){
+//        left(76);
+//      }
+//      else{
+//        stp();
+//        right_cubs[right_cube_counter] = true;
+//        right_cube_counter += 1;
+//        b4 = false;
+//        if (cur_perec_counter<7) b3 = true;
+//        else b5 = true;
+//        while (measureCm(f, f_cm)>right_circle_distance)right(85);
+//        right(100);
+//        delay(500);
+//        stp();
+//        delay(500);
+//      }
+//    }
+//  }
 }
